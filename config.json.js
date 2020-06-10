7c7f605bb31d13cc5cf8f32cabbfcc633fec517a
sadoneli <sadoneli@gmail.com>
2020-06-10 20:26:13 +0800

    add build files

---
+++ config.json.js
@@ -0,0 +1,8 @@
+{
+    "description": "易有云2.0 （LinkEase） 手机相册同步，PC 双向同步！", 
+    "home_url": "Module_linkease.asp", 
+    "md5": "ea3e4cb502822566204ebe90ab3ab592", 
+    "module": "linkease", 
+    "title": "易有云2.0", 
+    "version": "2.4"
+}
