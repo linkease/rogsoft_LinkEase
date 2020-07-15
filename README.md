48bf99add9ddc011637149008e142fdd37ea6eb6
janson <janson@gmail.com>
2020-07-15 14:50:07 +0800

    binary 0.2.86 version 2.7

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # rogsoft_LinkEase
+
+[更多信息请查看](https://www.ddnsto.com/linkease/)
+
--- config.json.js
+++ config.json.js
@@ -1,7 +1,7 @@
 {
     "description": "易有云2.0 （LinkEase） 手机相册同步，PC 双向同步！", 
     "home_url": "Module_linkease.asp", 
-    "md5": "7eee319df970d4f42aff549c4fff7d34", 
+    "md5": "c4e392a2013f88234a59ea020c53f61c", 
     "module": "linkease", 
     "title": "易有云2.0", 
     "version": "2.6"
