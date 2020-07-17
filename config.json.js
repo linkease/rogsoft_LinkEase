3a6cc126388e9b11577cf643f62eea76ee8051bc
janson <janson@gmail.com>
2020-07-17 17:23:01 +0800

    upgrade new binaries

--- config.json.js
+++ config.json.js
@@ -1,7 +1,7 @@
 {
     "description": "易有云2.0 （LinkEase） 手机相册同步，PC 双向同步！", 
     "home_url": "Module_linkease.asp", 
-    "md5": "d4cd4b6ac724f4ed24adae1e26cdb757", 
+    "md5": "02920d1276b5d6f33aced87eb9ddc01a", 
     "module": "linkease", 
     "title": "易有云2.0", 
     "version": "2.7.1"
