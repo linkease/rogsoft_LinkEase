e457b3ff5267e0a3dbfc463127f8296d4d42e547
janson <janson@gmail.com>
2020-08-19 22:34:03 +0800

    disable image in router

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
     "description": "易有云2.0 （LinkEase） 手机相册同步，PC 双向同步！", 
     "home_url": "Module_linkease.asp", 
-    "md5": "d72e64c1c7419d3d699d2e97fc51651b", 
+    "md5": "1beb464c86004bebed6e4cc95830a6e7", 
     "module": "linkease", 
     "title": "易有云2.0", 
-    "version": "2.7.3"
+    "version": "2.7.6"
 }
