a06f7df7bf39230d8e3a5abd46d3a9f1052da7ed
sadoneli <sadoneli@gmail.com>
2020-09-17 10:51:05 +0800

    support for ZenWiFi_XD4

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
     "description": "易有云2.0 （LinkEase） 手机相册同步，远程管文件，看视频！", 
     "home_url": "Module_linkease.asp", 
-    "md5": "6704bca70c5818c02a136b0cb6d4cd1f", 
+    "md5": "5423e01a47f8e0c4a824618ba7666898", 
     "module": "linkease", 
     "title": "易有云2.0", 
-    "version": "2.9.2"
+    "version": "2.9.3"
 }
