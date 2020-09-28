0c19bb25fa14866fe9751e02315fc20623b5d394
sadoneli <sadoneli@gmail.com>
2020-09-28 21:06:31 +0800

    update

--- config.json.js
+++ config.json.js
@@ -1,7 +1,7 @@
 {
     "description": "易有云2.0 （LinkEase） 手机相册同步，远程管文件，看视频！", 
     "home_url": "Module_linkease.asp", 
-    "md5": "5423e01a47f8e0c4a824618ba7666898", 
+    "md5": "d7d6ff30f2f0c947e80722c8843b8fb4", 
     "module": "linkease", 
     "title": "易有云2.0", 
     "version": "2.9.3"
