849737c85822778c3271c7c9fe8dfd002e6e46eb
janson <janson@gmail.com>
2023-02-01 17:52:32 +0800

    upgrade linkease

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
     "description": "易有云2.0 （LinkEase） 手机相册同步，远程管文件，看视频！", 
     "home_url": "Module_linkease.asp", 
-    "md5": "719cdb5e7acd98602a11794598c6d796", 
+    "md5": "4f5e70c4a5c5d14ed3bfd11493f8b51c", 
     "module": "linkease", 
     "title": "易有云2.0", 
-    "version": "2.10.7"
+    "version": "2.11.0"
 }
