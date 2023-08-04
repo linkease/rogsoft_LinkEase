1363a61e89e4ed6c97e4bdfaaf806b5e6d4ace60
janson <janson@gmail.com>
2023-08-04 11:27:02 +0800

    bump to version 1.2.2

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
     "description": "易有云 （LinkEase） 手机相册同步，远程管文件，看视频！", 
     "home_url": "Module_linkease.asp", 
-    "md5": "a2ef76538ed1e6e0bf6015e4429646a9", 
+    "md5": "55babf8305b4bc1c37af0721d3e776d3", 
     "module": "linkease", 
     "title": "易有云", 
-    "version": "2.12.0"
+    "version": "2.12.2"
 }
