0c49f7ca9818e5868be7a7d37e4ed39432f8bd4e
janson <janson@gmail.com>
2024-07-22 15:15:01 +0800

    fix linkease binary error

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
     "description": "易有云 （LinkEase） 手机相册同步，远程管文件，看视频！", 
     "home_url": "Module_linkease.asp", 
-    "md5": "2c7399c9d01a47dde1cb54ae301bbb6b", 
+    "md5": "8a10d044f95a85f1cc8c0486a46552c4", 
     "module": "linkease", 
     "title": "易有云", 
-    "version": "2.16.5"
+    "version": "2.16.6"
 }
