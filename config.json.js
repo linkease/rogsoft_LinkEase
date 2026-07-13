87967c24d36ee31a797313bc23651cfebe9b3c83
jannson <jannon.wei@gmail.com>
2026-07-13 03:31:11 +0000

    chore: rebuild linkease package after hardening

--- config.json.js
+++ config.json.js
@@ -3,7 +3,7 @@
     "full_artifact_sha256": "5a9b8a613ff6746dbfcac0afa8a2d22186f85f73a6bac8d26a8b042d218147b9",
     "full_artifact_url": "https://github.com/linkease/linkease-desktop/releases/download/linkease-full-v3.0.0/linkease-full-binary-3.0.0.tar.gz",
     "home_url": "Module_linkease.asp",
-    "md5": "b0f278758ab7656728cab2e3cea5aefb",
+    "md5": "d38641414e5ca683ebc2d35aa4c83614",
     "module": "linkease",
     "title": "易有云",
     "version": "2.17.5"
