c1c76a0ffaf30ab55d5f3f136a3cef5d22df6a41
jannson <jannon.wei@gmail.com>
2026-07-13 03:06:49 +0000

    chore: rebuild linkease asus package

--- config.json.js
+++ config.json.js
@@ -3,7 +3,7 @@
     "full_artifact_sha256": "6ae7ddbe28ca07e9e2a51476b0ae7ffdef1d1fa3d4f9b48260be700c1fff0833",
     "full_artifact_url": "https://github.com/linkease/linkease-desktop/releases/download/prebuild/linkease-asus-full-arm64-v3.0.0.tar.gz",
     "home_url": "Module_linkease.asp",
-    "md5": "c842e51b52e04d90574c7c64b2cc355c",
+    "md5": "6038ff2ee21f1106c3fa638546e6eb0d",
     "module": "linkease",
     "title": "易有云",
     "version": "2.17.5"
