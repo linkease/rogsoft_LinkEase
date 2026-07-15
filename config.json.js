a939e48a6ff374bb1fca30348d031ee1d69ed99f
jannson <jannon.wei@gmail.com>
2026-07-15 16:09:56 +0000

    release: update linkease full 2.17.7

--- config.json.js
+++ config.json.js
@@ -1,10 +1,10 @@
 {
     "description": "易有云 （LinkEase） 手机相册同步，远程管文件，看视频！",
-    "full_artifact_sha256": "415fbb73949b3b42af4b7812402aa75e813266d5e9a22428c399df16ddb76144",
-    "full_artifact_url": "https://fw0.koolcenter.com/binary/LinkEase/LinkEaseFull/linkease-full-binary-3.0.1.tar.gz",
+    "full_artifact_sha256": "bca98262a50c938fce55539ac75c3e309f059d608e49ea11221f83c91a2a815e",
+    "full_artifact_url": "file:///mnt/sata1-1/firmware/asusgo-build/local-artifacts/linkease/linkease-full-binary-2.17.7.tar.gz",
     "home_url": "Module_linkease.asp",
-    "md5": "999fdb03bff179bdd5d077e6cdae707a",
+    "md5": "a4259f28db1450faa10c302a124efcf7",
     "module": "linkease",
     "title": "易有云",
-    "version": "2.17.6"
+    "version": "2.17.7"
 }
