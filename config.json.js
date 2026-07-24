9fce984db11c59e93459a81ab70bd116f23b71d0
jannson <jannon.wei@gmail.com>
2026-07-24 03:30:10 +0000

    Use LinkEase runtime bundle 3.0.4

--- config.json.js
+++ config.json.js
@@ -1,9 +1,9 @@
 {
     "description": "易有云 （LinkEase） 手机相册同步，远程管文件，看视频！",
-    "full_artifact_sha256": "dbd980e99887b482c6eed4e561dcd8d56c573e686d7e3f8a9437a946f7d85540",
-    "full_artifact_url": "file:///projects/workspace-linkease-ubuntu/linkease-github/linkease-desktop/release-runtime/3.0.3-326d25a-92b28c5-official/linkease-runtime-3.0.3-linux-arm32.tar.gz",
+    "full_artifact_sha256": "5b0d134ad95fa4b12af71f6fb47c791aa8813a55d68a8392dde07349ac1d257d",
+    "full_artifact_url": "file:///projects/workspace-linkease-ubuntu/linkease-github/linkease-desktop/release-runtime/3.0.4-6a1688e-92b28c5-official/linkease-runtime-3.0.4-linux-arm32.tar.gz",
     "home_url": "Module_linkease.asp",
-    "md5": "58bf59f75f0ad505c6a45fa4b5d4ca58",
+    "md5": "ee0a7357c972da6675386529eb60540c",
     "module": "linkease",
     "title": "易有云",
     "version": "3.0.4"
