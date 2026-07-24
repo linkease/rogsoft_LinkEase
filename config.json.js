e350c70a49e3778bbcd23d723cd2491a859fe56b
jannson <jannon.wei@gmail.com>
2026-07-24 03:33:32 +0000

    Record LinkEase runtime 3.0.4 package checksum

--- config.json.js
+++ config.json.js
@@ -3,7 +3,7 @@
     "full_artifact_sha256": "5b0d134ad95fa4b12af71f6fb47c791aa8813a55d68a8392dde07349ac1d257d",
     "full_artifact_url": "file:///projects/workspace-linkease-ubuntu/linkease-github/linkease-desktop/release-runtime/3.0.4-6a1688e-92b28c5-official/linkease-runtime-3.0.4-linux-arm32.tar.gz",
     "home_url": "Module_linkease.asp",
-    "md5": "ee0a7357c972da6675386529eb60540c",
+    "md5": "8a37b51ceb8b3440c6163e96f77c56a0",
     "module": "linkease",
     "title": "易有云",
     "version": "3.0.4"
