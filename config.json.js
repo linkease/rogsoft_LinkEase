b0cc22c785fc872a3a4583eeb6ce4b433c8de3b6
jannson <jannon.wei@gmail.com>
2026-07-24 03:09:24 +0000

    Bump LinkEase ASUSWRT package to 3.0.4

--- config.json.js
+++ config.json.js
@@ -1,10 +1,10 @@
 {
     "description": "易有云 （LinkEase） 手机相册同步，远程管文件，看视频！",
-    "full_artifact_sha256": "3c04e9dcd8738408339c3edecdb1bb4584eb43a6ff23ddd24ac09c9b4c30806e",
-    "full_artifact_url": "file:///projects/workspace-linkease-ubuntu/linkease-github/linkease-desktop/release-runtime/3.0.3-326d25a-92b28c5-official/linkease-runtime-3.0.3-linux-arm64.tar.gz",
+    "full_artifact_sha256": "dbd980e99887b482c6eed4e561dcd8d56c573e686d7e3f8a9437a946f7d85540",
+    "full_artifact_url": "file:///projects/workspace-linkease-ubuntu/linkease-github/linkease-desktop/release-runtime/3.0.3-326d25a-92b28c5-official/linkease-runtime-3.0.3-linux-arm32.tar.gz",
     "home_url": "Module_linkease.asp",
-    "md5": "efccb9e05380124a44a603394ebe0571",
+    "md5": "32556f25cd2ed6cdcd0955d84cb8ed76",
     "module": "linkease",
     "title": "易有云",
-    "version": "3.0.3"
+    "version": "3.0.4"
 }
